bad945012bb07df322719f54957fd6ad21051f5c
Joanna Gora <joanna.gora@protonmail.com>
2018-08-03 14:51:16 +0100

    tested hotelList getter for hotel

--- src/main/java/Hotels/Hotel.java
+++ src/main/java/Hotels/Hotel.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+package Hotels;
+import Rooms.Room;
+import java.util.ArrayList;
 
-public class Hotel {
+public class Hotel{
+
+    private ArrayList<Room> roomList;
+
+    public Hotel(){
+       this.roomList = new ArrayList<>();
+    }
+
+    public ArrayList<Room> getRoomList(){
+        return roomList;
+    }
 }
--- src/test/java/HotelTest.java
+++ src/test/java/HotelTest.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import Hotels.Hotel;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class HotelTest {
+
+    Hotel hotel;
+    @Before
+    public void before(){
+        hotel = new Hotel();
+    }
+
+    @Test
+    public void hasRoomList(){
+        assertEquals(0, hotel.getRoomList().size());
+    }
 }
